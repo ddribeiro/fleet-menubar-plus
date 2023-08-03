8d1e85d33866cd9be5e51bf3680913d28acb7863
Dale Ribeiro <dale@harmonize.io>
2023-08-03 14:56:04 -0400

    host data model

---
+++ HarmonizeMenuBarExtra/Models/Host.swift
@@ -0,0 +1,13 @@
+//
+//  Host.swift
+//  HarmonizeMenuBarExtra
+//
+//  Created by Dale Ribeiro on 8/3/23.
+//
+
+import Foundation
+
+struct Host: Identifiable, Codable {
+    var id: Int
+    var name: String
+}
